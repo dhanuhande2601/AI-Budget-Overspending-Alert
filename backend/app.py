e2bf347f6499465b71b31194b4c73cea2e643ac6
Your Name <your_email@example.com>
2026-06-19 16:16:32 +0530

    Fix Netlify origin

--- backend/app.py
+++ backend/app.py
@@ -88,7 +88,7 @@ CORS(
             "origins": [
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
-                "https://subtle-crumble-543032.netlify.app/"
+                "https://subtle-crumble-543032.netlify.app"
             ],
             "methods": [
                 "GET",
